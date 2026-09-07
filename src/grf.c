937eb9c3491619575cf0b389ee8bcfd5dfa3c772
Garhoogin <declan.c.moore@gmail.com>
2026-09-06 23:47:51 -0500

    Clean up GRF writer a bit

--- src/grf.c
+++ src/grf.c
@@ -11,8 +11,8 @@
 
 // ----- GRF output code
 
-static unsigned int GrfWrite(FILE *fp, const void *p, unsigned int size) {
-	return fwrite(p, 1, size, fp);
+static int GrfWrite(FILE *fp, const void *p, unsigned int size) {
+	return fwrite(p, 1, size, fp) == size;
 }
 
 static int GrfEmitBlockHeader(FILE *fp, uint32_t signature, uint32_t size) {
@@ -22,13 +22,45 @@ static int GrfEmitBlockHeader(FILE *fp, uint32_t signature, uint32_t size) {
 	GrfBinaryBlockHeader header;
 	header.signature = signature;
 	header.size = size;
-	return GrfWrite(fp, &header, sizeof(header)) == sizeof(header);
+	return GrfWrite(fp, &header, sizeof(header));
 }
 
 static int GrfAlignBlock(FILE *fp, unsigned int dataSize) {
 	unsigned int alignment = (4 - (dataSize & 3)) & 3;
 	unsigned char pad[3] = { 0, 0, 0 };
-	return GrfWrite(fp, pad, alignment) == alignment;
+	return GrfWrite(fp, pad, alignment);
+}
+
+static int GrfWriteBlock(
+	FILE        *fp,
+	uint32_t     tag,
+	const void  *data,
+	unsigned int size
+) {
+	//emit the block header, data, and alignment padding.
+	if (!GrfEmitBlockHeader(fp, tag, size)) return 0;
+	if (!GrfWrite(fp, data, size))          return 0;
+	if (!GrfAlignBlock(fp, size))           return 0;
+	
+	return 1;
+}
+
+static int GrfWriteBlockComp(
+	FILE               *fp,
+	uint32_t            tag,
+	const void         *data,
+	unsigned int        size,
+	CxCompressionPolicy compress
+) {
+	unsigned int compSize;
+	void *compData = CxCompress(data, size, &compSize, compress);
+	if (compData == NULL) return 0;
+	
+	//write data block block
+	int status = GrfWriteBlock(fp, tag, compData, compSize);
+	free(compData);
+	
+	return status;
 }
 
 
@@ -61,7 +93,7 @@ int GrfWriteHeader(FILE *fp) {
 	header.signature = GRF_MKTAG('R', 'I', 'F', 'F');
 	header.fileIdentifier = GRF_MKTAG('G', 'R', 'F', ' ');
 	header.fileSize = sizeof(header);
-	return GrfWrite(fp, &header, sizeof(header)) == sizeof(header);
+	return GrfWrite(fp, &header, sizeof(header));
 }
 
 int GrfWriteHdr(
@@ -99,10 +131,7 @@ int GrfWriteHdr(
 	fileHeader.gfxWidth = gfxWidth;
 	fileHeader.gfxHeight = gfxHeight;
 	
-	int status = 1;
-	if (status) status = GrfEmitBlockHeader(fp, GRF_TAG_HDRX, sizeof(fileHeader));
-	if (status) status = GrfWrite(fp, &fileHeader, sizeof(fileHeader));
-	return status;
+	return GrfWriteBlock(fp, GRF_TAG_HDRX, &fileHeader, sizeof(fileHeader));
 }
 
 int GrfBgWriteHdr(
@@ -129,13 +158,13 @@ int GrfTexWriteHdr(
 	//convert texture format into what GRF expects
 	int gfxAttr = 0;
 	switch (fmt) {
-		case CT_4COLOR:   gfxAttr = GRF_GFX_ATTR_2BIT;   break;
-		case CT_16COLOR:  gfxAttr = GRF_GFX_ATTR_4BIT;   break;
-		case CT_256COLOR: gfxAttr = GRF_GFX_ATTR_8BIT;   break;
-		case CT_A3I5:     gfxAttr = GRF_GFX_ATTR_A3I5;   c0xp = 0; break;
-		case CT_A5I3:     gfxAttr = GRF_GFX_ATTR_A5I3;   c0xp = 0; break;
-		case CT_DIRECT:   gfxAttr = GRF_GFX_ATTR_16BIT;  c0xp = 0; break;
-		case CT_4x4:      gfxAttr = GRF_GFX_ATTR_TEX4x4; c0xp = 0; break;
+		case GX_TEXFMT_PLTT4   : gfxAttr = GRF_GFX_ATTR_2BIT;   break;
+		case GX_TEXFMT_PLTT16  : gfxAttr = GRF_GFX_ATTR_4BIT;   break;
+		case GX_TEXFMT_PLTT256 : gfxAttr = GRF_GFX_ATTR_8BIT;   break;
+		case GX_TEXFMT_A3I5    : gfxAttr = GRF_GFX_ATTR_A3I5;   c0xp = 0; break;
+		case GX_TEXFMT_A5I3    : gfxAttr = GRF_GFX_ATTR_A5I3;   c0xp = 0; break;
+		case GX_TEXFMT_DIRECT  : gfxAttr = GRF_GFX_ATTR_16BIT;  c0xp = 0; break;
+		case GX_TEXFMT_TEX4x4  : gfxAttr = GRF_GFX_ATTR_TEX4x4; c0xp = 0; break;
 	}
 	
 	GrfGfxFlags flags = 0;
@@ -147,49 +176,34 @@ int GrfTexWriteHdr(
 	return GrfWriteHdr(fp, gfxAttr, GRF_SCREEN_TYPE_NONE, 0, paletteSize, tileSize, tileSize, 0, 0, flags, width, height);
 }
 
-int GrfWritePltt(FILE *fp, const void *data, unsigned int nColors, CxCompressionPolicy compress) {
+int GrfWritePltt(
+	FILE               *fp,
+	const void         *data,
+	unsigned int        nColors,
+	CxCompressionPolicy compress
+) {
 	//encapsulate the data in a compression header
-	unsigned int dataSize;
-	void *compData = CxCompress(data, nColors * 2, &dataSize, compress);
-	if (compData == NULL) return 0;
-	
-	int status = 1;
-	if (status) status = GrfEmitBlockHeader(fp, GRF_TAG_PAL, dataSize);
-	if (status) status = GrfWrite(fp, compData, dataSize);
-	if (status) status = GrfAlignBlock(fp, dataSize);
-	free(compData);
-	
-	return status;
+	return GrfWriteBlockComp(fp, GRF_TAG_PAL, data, nColors * 2, compress);
 }
 
-int GrfWriteGfx(FILE *fp, const void *data, unsigned int size, CxCompressionPolicy compress) {
+int GrfWriteGfx(
+	FILE               *fp,
+	const void         *data,
+	unsigned int        size,
+	CxCompressionPolicy compress
+) {
 	//encapsulate the data in a compression header
-	unsigned int dataSize;
-	void *compData = CxCompress(data, size, &dataSize, compress);
-	if (compData == NULL) return 0;
-	
-	int status = 1;
-	if (status) status = GrfEmitBlockHeader(fp, GRF_TAG_GFX, dataSize);
-	if (status) status = GrfWrite(fp, compData, dataSize);
-	if (status) status = GrfAlignBlock(fp, dataSize);
-	free(compData);
-	
-	return status;
+	return GrfWriteBlockComp(fp, GRF_TAG_GFX, data, size, compress);
 }
 
-int GrfWriteScr(FILE *fp, const void *data, unsigned int size, CxCompressionPolicy compress) {
+int GrfWriteScr(
+	FILE               *fp,
+	const void         *data,
+	unsigned int        size,
+	CxCompressionPolicy compress
+) {
 	//encapsulate the data in a compression header
-	unsigned int dataSize;
-	void *compData = CxCompress(data, size, &dataSize, compress);
-	if (compData == NULL) return 0;
-	
-	int status = 1;
-	if (status) status = GrfEmitBlockHeader(fp, GRF_TAG_MAP, dataSize);
-	if (status) status = GrfWrite(fp, compData, dataSize);
-	if (status) status = GrfAlignBlock(fp, dataSize);
-	free(compData);
-	
-	return status;
+	return GrfWriteBlockComp(fp, GRF_TAG_MAP, data, size, compress);
 }
 
 int GrfWriteTexImage(
@@ -200,34 +214,19 @@ int GrfWriteTexImage(
 	unsigned int          pidxSize,
 	CxCompressionPolicy   compress
 ) {
-	//texture image data encapsulated in GFX block. 
-	unsigned int dataSize;
-	void *compData = CxCompress(txel, txelSize, &dataSize, compress);
-	if (compData == NULL) return 0;
-	
-	//write GFX block
-	int status = 1;
-	if (status) status = GrfEmitBlockHeader(fp, GRF_TAG_GFX, dataSize);
-	if (status) status = GrfWrite(fp, compData, dataSize);
-	if (status) status = GrfAlignBlock(fp, dataSize);
-	free(compData);
+	//texture image data encapsulated in GFX block.
+	int status = GrfWriteBlockComp(fp, GRF_TAG_GFX, txel, txelSize, compress);
 	
 	//write PIDX block
 	if (pidx != NULL && status) {
-		unsigned int dataSizePidx;
-		void *pidxComp = CxCompress(pidx, pidxSize, &dataSizePidx, compress);
-		if (pidxComp == NULL) status = 0;
-		
-		if (status) status = GrfEmitBlockHeader(fp, GRF_TAG_PIDX, dataSizePidx);
-		if (status) status = GrfWrite(fp, pidxComp, dataSizePidx);
-		if (status) status = GrfAlignBlock(fp, dataSizePidx);
-		if (pidxComp != NULL) free(pidxComp);
+		status = GrfWriteBlockComp(fp, GRF_TAG_PIDX, pidx, pidxSize, compress);
 	}
 	
 	return status;
 }
 
 int GrfFinalize(FILE *fp) {
+	//seek to the start of the file and write the correct data size
 	uint32_t pos = ftell(fp) - 8;
 	fseek(fp, offsetof(GrfFileHeader, fileSize), SEEK_SET);
 	fwrite(&pos, 1, sizeof(pos), fp);
